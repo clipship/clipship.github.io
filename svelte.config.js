abc2d0eaf624aec741191dede6302fc8a2ee906e
doodlezucc <electerminator@gmail.com>
2025-05-14 19:53:10 +0200

    Add workflow for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,18 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		})
+	},
+
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	}
 };
 
